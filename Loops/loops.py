6f6d6c5628a5b51f578d3e881ebc7a4667e14372
Vivek18 <viveksaha096@gmail.com>
2025-11-09 11:44:49 +0530

    Loops in Python

---
+++ Loops/loops.py
@@ -0,0 +1,22 @@
+# For Loop
+
+for i in range(5):
+    print("For Loop Iteration", i)
+
+
+# While Loop
+i = 1
+while i <= 5:
+    print("While Loop Iteration", i)
+    i += 1
+
+# Nested Loop
+for i in range(3):
+    for j in range(2):
+        print("Nested Loop Iteration", i, j)
+
+
+list = [10, 20, 30, 40, 50]
+for item in list:
+    print("List Item:", item)
+
