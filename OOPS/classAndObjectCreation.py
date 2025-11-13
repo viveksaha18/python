06e8d4500503b900a9e286a7551e7a5f28b7c661
Vivek18 <viveksaha096@gmail.com>
2025-11-13 11:25:15 +0530

    Class And Object Creation

---
+++ OOPS/classAndObjectCreation.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+    
+
+# Creating an object of the Student class
+s1 = Student("Vivek", 21)
+print(s1.name)  # Output: Vivek
+print(s1.age)   # Output: 21
+
+class Car:
+    def __init__(self, brand, model):
+        carBrand = brand
+        carModel = model
+        print(carBrand)
+        print(carModel)
+
+# Creating an object of the Car class
+c1 = Car("Toyota", "Corolla")
