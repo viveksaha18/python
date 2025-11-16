346fa6d74ec24252bec9cd540ddc1e04829a0490
Vivek18 <viveksaha096@gmail.com>
2025-11-16 18:32:40 +0530

    Templates and Errors in Django

---
+++ Django/chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    return render(request, 'index.html')
+def about(request):
+    return HttpResponse("This is the About Page.")  
+
+def contact(request):
+    return HttpResponse("Contact us ")
